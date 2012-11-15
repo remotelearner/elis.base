05f3def5d826d07f95462a63a7e7cbe1c7452e23
Brent Boghosian <brent.boghosian@remote-learner.net>
2012-11-15 13:58:00 -0500

    ELIS 2.2.0.11 release

--- core/admin/tool/phpunittest/settings.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$ADMIN->add('reports', new admin_externalpage('tool_phpunittest', get_string('pluginname', 'tool_phpunittest'),
-            "$CFG->wwwroot/$CFG->admin/tool/phpunittest/index.php", 'tool/phpunittest:view'));
--- core/elis/core/plugins/user_activity/lang/en/eliscoreplugins_user_activity.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$string['pluginname'] = 'User Activity ETL';
