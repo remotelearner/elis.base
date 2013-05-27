6bd5b3981d0e52b5b07798ed35acc04b73653008
Justin Filip <jfilip@remote-learner.ca>
2013-05-27 12:38:11 -0400

    ELIS 2.4.1.1 release

--- core/elis/core/plugins/user_activity/version.php
+++ core/elis/core/plugins/user_activity/version.php
@@ -3,4 +3,4 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2013031500;
-$plugin->release = '2.4.1.0 (Build: 20130322)';
+$plugin->release = '2.4.1.1 (Build: 20130513)';
--- core/elis/core/version.php
+++ core/elis/core/version.php
@@ -27,4 +27,4 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2013031500;
-$plugin->release = '2.4.1.0 (Build: 20130322)';
+$plugin->release = '2.4.1.1 (Build: 20130513)';
