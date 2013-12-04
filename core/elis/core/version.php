65b6216dc50d9d03b0815a235f64c035850b622d
Brent Boghosian <brent.boghosian@remote-learner.net>
2013-12-04 16:37:26 -0500

    ELIS 2.4.3.5 release

--- core/elis/core/version.php
+++ core/elis/core/version.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * ELIS(TM): Enterprise Learning Intelligence Suite
- * Copyright (C) 2008-2013 Remote Learner.net Inc http://www.remote-learner.net
+ * Copyright (C) 2008-2013 Remote Learner.net Inc (http://www.remote-learner.net)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,15 +16,14 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @package    elis
- * @subpackage core
+ * @package    elis_core
  * @author     Remote-Learner.net Inc
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @copyright  (C) 2008-2013 Remote Learner.net Inc http://www.remote-learner.net
  *
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2013051403;
-$plugin->release = '2.4.3.3 (Build: 20131023)';
+$plugin->version = 2013051405;
+$plugin->release = '2.4.3.5 (Build: 20131203)';
